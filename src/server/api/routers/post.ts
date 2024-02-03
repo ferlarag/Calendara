775c656df3dfaac8801172a091dc400ce60421ab
ferlarag <89590031+ferlarag@users.noreply.github.com>
2024-02-03 09:10:44 -0600

    fix: remove sample router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -10,23 +10,4 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
 });
