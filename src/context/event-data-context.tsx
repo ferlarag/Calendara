efe9d8ede6f514cefe79290e60bb5a24dd7c4960
ferlarag <89590031+ferlarag@users.noreply.github.com>
2024-02-05 14:29:37 -0600

    feat: create reducer to store useEventData state

--- src/context/event-data-context.tsx
+++ src/context/event-data-context.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import { ReactNode, createContext, useState } from "react";
+import { type EventInformation } from "@/types/event-information";
+import { EventColors } from "@prisma/client";
+import { type ReactNode, createContext, useReducer, useState } from "react";
+import eventInformationReducer from "./edit-information-reducer";
+
+const eventInformationInitialState: EventInformation = {
+  name: "",
+  color: EventColors.LIGHT_BLUE_SKY,
+  link: "my-new-event", //generate-random-string?
+  duration: 30,
+  description: "",
+  locations: [],
+};
 
 export interface EventDataContextType {
   currentWindow: EditEventWindow;
   changeWindow: (newWindow: EditEventWindow) => void;
+  eventInformation: EventInformation;
+  changeEventColor: (newColor: EventColors) => void;
+  changeName: (newName: string) => void;
+  changeEventLink: (newUrl: string) => void;
+  changeDescription: (newDescription: string) => void;
 }
 
 export const EventDataContext = createContext<EventDataContextType | undefined>(
@@ -21,6 +38,11 @@ export enum EditEventWindow {
 }
 
 export const EventDataProvider = ({ children }: { children: ReactNode }) => {
+  const [eventInformation, dispatchEventInformation] = useReducer(
+    eventInformationReducer,
+    eventInformationInitialState,
+  );
+
   const [currentWindow, setCurrentWindow] = useState<EditEventWindow>(
     EditEventWindow.HOME,
   );
@@ -29,8 +51,34 @@ export const EventDataProvider = ({ children }: { children: ReactNode }) => {
     setCurrentWindow(newWidow);
   };
 
+  const changeEventColor = (newColor: EventColors) => {
+    dispatchEventInformation({ type: "CHANGE_COLOR", newColor });
+  };
+
+  const changeName = (newName: string) => {
+    dispatchEventInformation({ type: "CHANGE_NAME", newName });
+  };
+
+  const changeEventLink = (newUrl: string) => {
+    dispatchEventInformation({ type: "CHANGE_URL", newUrl });
+  };
+
+  const changeDescription = (newDescription: string) => {
+    dispatchEventInformation({ type: "CHANGE_DESCRIPTION", newDescription });
+  };
+
   return (
-    <EventDataContext.Provider value={{ currentWindow, changeWindow }}>
+    <EventDataContext.Provider
+      value={{
+        currentWindow,
+        changeWindow,
+        eventInformation,
+        changeEventColor,
+        changeName,
+        changeEventLink,
+        changeDescription,
+      }}
+    >
       {children}
     </EventDataContext.Provider>
   );
